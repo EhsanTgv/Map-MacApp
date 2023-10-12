0c0d2517861af26ef8da9889b417e6b32af5a22c
EhsanTgv <EhsanTgv@gmail.com>
2023-10-12 14:03:35 +0300

    Initial Commit

---
+++ Map-MacApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Map-MacApp
+//
+//  Created by Ehsan Taghavi on 12.10.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
